3ca867f1a0b5d352baa90ad2a2a7020d9a6753b0
roderi18 <rdpr18@gmail.com>
2026-04-28 21:30:18 -0400

    Restore member and level view exports

--- src/sections/member/view/member-cards-view.jsx
+++ src/sections/member/view/member-cards-view.jsx
@@ -1,44 +1,44 @@
-// 'use client';
+'use client';
 
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 
-// import { paths } from 'src/routes/paths';
-// import { RouterLink } from 'src/routes/components';
+import { paths } from 'src/routes/paths';
+import { RouterLink } from 'src/routes/components';
 
-// import { _memberCards } from 'src/_mock';
-// import { DashboardContent } from 'src/layouts/dashboard';
+import { _memberCards } from 'src/_mock';
+import { DashboardContent } from 'src/layouts/dashboard';
 
-// import { Iconify } from 'src/components/iconify';
-// import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
+import { Iconify } from 'src/components/iconify';
+import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
 
-// import { MemberCardList } from '../member-card-list';
+import { MemberCardList } from '../member-card-list';
 
-// // ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 
-// export function MemberCardsView() {
-//   return (
-//     <DashboardContent>
-//       <CustomBreadcrumbs
-//         heading="Cards"
-//         links={[
-//           { name: 'Panel', href: paths.dashboard.root },
-//           { name: 'Miembros', href: paths.dashboard.level.member.root },
-//           { name: 'Cards' },
-//         ]}
-//         action={
-//           <Button
-//             component={RouterLink}
-//             href={paths.dashboard.level.member.new}
-//             variant="contained"
-//             startIcon={<Iconify icon="mingcute:add-line" />}
-//           >
-//             Agregar miembro
-//           </Button>
-//         }
-//         sx={{ mb: { xs: 3, md: 5 } }}
-//       />
+export function MemberCardsView() {
+  return (
+    <DashboardContent>
+      <CustomBreadcrumbs
+        heading="Cards"
+        links={[
+          { name: 'Panel', href: paths.dashboard.root },
+          { name: 'Miembros', href: paths.dashboard.level.member.root },
+          { name: 'Cards' },
+        ]}
+        action={
+          <Button
+            component={RouterLink}
+            href={paths.dashboard.level.member.new}
+            variant="contained"
+            startIcon={<Iconify icon="mingcute:add-line" />}
+          >
+            Agregar miembro
+          </Button>
+        }
+        sx={{ mb: { xs: 3, md: 5 } }}
+      />
 
-//       <MemberCardList members={_memberCards} />
-//     </DashboardContent>
-//   );
-// }
+      <MemberCardList members={_memberCards} />
+    </DashboardContent>
+  );
+}
