22497f4dfcd729c17a88df13fded174a93379f25
roderi18 <rdpr18@gmail.com>
2026-04-29 22:38:23 -0400

    Route sign in button to auth pages

--- src/layouts/components/sign-in-button.jsx
+++ src/layouts/components/sign-in-button.jsx
@@ -1,5 +1,6 @@
 import Button from '@mui/material/Button';
 
+import { paths } from 'src/routes/paths';
 import { RouterLink } from 'src/routes/components';
 
 import { CONFIG } from 'src/global-config';
@@ -7,14 +8,15 @@ import { CONFIG } from 'src/global-config';
 // ----------------------------------------------------------------------
 
 export function SignInButton({ sx, ...other }) {
+  const href =
+    (CONFIG.auth.method === 'firebase' && paths.auth.firebase.signIn) ||
+    (CONFIG.auth.method === 'supabase' && paths.auth.supabase.signIn) ||
+    (CONFIG.auth.method === 'amplify' && paths.auth.amplify.signIn) ||
+    (CONFIG.auth.method === 'auth0' && paths.auth.auth0.signIn) ||
+    paths.auth.jwt.signIn;
+
   return (
-    <Button
-      component={RouterLink}
-      href={CONFIG.auth.redirectPath}
-      variant="outlined"
-      sx={sx}
-      {...other}
-    >
+    <Button component={RouterLink} href={href} variant="outlined" sx={sx} {...other}>
       Sign in
     </Button>
   );
